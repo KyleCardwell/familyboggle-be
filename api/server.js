a49456ffc8f089ceed166c69dc1adcfccf737dde
Kyle <kyle.e.cardwell@gmail.com>
2022-01-30 14:44:09 -0700

    server to use helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const helmet = require('helmet')
+const cors = require('cors')
 
 const server = express();
 
 const BoggleLettersRouter = require('./boggleLetters/boggleLettersRouter')
 
 server.use(express.json());
+server.use(helmet())
+server.use(cors())
 
 server.use('/api/boggleLetters', BoggleLettersRouter);
 
