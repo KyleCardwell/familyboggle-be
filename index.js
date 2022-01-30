5314ff642f2edf51da1c5e7a89b6eb118dcf9cc8
Kyle <kyle.e.cardwell@gmail.com>
2022-01-30 13:20:13 -0700

    index and server setup, added node-boggle-solver

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const server = require('./api/server.js')
+
+const port = process.env.PORT || 5000;
+
+server.USE('*', (req, res) => {
+    res.send('<h1>Boggle Solver Loaded</h1>')
+})
+
+
+server.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
